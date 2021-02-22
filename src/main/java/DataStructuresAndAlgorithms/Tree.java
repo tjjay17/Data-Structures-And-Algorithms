a6bbf4b14a90d626d1ef9f63799c7e463594de17
Thanooj <thanoojjay@gmail.com>
2021-02-22 08:25:56 -0500

    Tree:DFS & BFS:pre and post orders

--- src/main/java/DataStructuresAndAlgorithms/Tree.java
+++ src/main/java/DataStructuresAndAlgorithms/Tree.java
@@ -62,18 +62,49 @@ public class Tree {
        return visited;
     }
     
-//    @Override
-//    public String toString(){
-//        System.out.println(this.root);
-//    }
+    ArrayList <Integer> preOrderStored = new ArrayList<Integer>();
+    public ArrayList <Integer> preorderDFS(tNode node){
+        preOrderStored.add(node.value);
+        if(node.left != null){
+            preorderDFS(node.left);
+        }
+        if(node.right != null){
+            preorderDFS(node.right);
+        }
+        return preOrderStored;
+    }
     
+    ArrayList <Integer> postOrderStored = new ArrayList <Integer>();
+    public ArrayList <Integer> postOrderDFS(tNode node){
+        if(node.left != null){
+           postOrderDFS(node.left);
+        }
+        
+        if(node.right != null){
+            postOrderDFS(node.right);
+        }
+        
+        postOrderStored.add(node.value);
+        return postOrderStored;
+    }
+        
     public static void main(String [] args){
         tNode root = new tNode(4);
+        tNode root2 = new tNode(10);
+        root2.left = new tNode(8);
+        root2.right = new tNode(12);
+        root2.left.left = new tNode(3);
+        root2.left.right = new tNode(9);
+        root2.right.left = new tNode(11);
+        root2.right.right = new tNode(14);
         Tree tree = new Tree(root);
+        Tree tree2 = new Tree(root2);
         root.left = new tNode(2);
         root.right = new tNode(8);
         root.right.right = new tNode(12);
         root.right.left = new tNode(6);
         System.out.println(tree.BFS());
+        System.out.println(tree2.preorderDFS(root2));
+        System.out.println(tree2.postOrderDFS(root2));
     }
 }
